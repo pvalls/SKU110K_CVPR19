63186e2606c2062fb134aa635572997cf237ca71
polvalls <polvallsrue@gmail.com>
2020-01-09 11:57:14 +0100

    refactor: changed the csv results directory to be 'detection_results/' within the same directory as the csv and the input images

--- object_detector_retinanet/keras_retinanet/utils/predict_iou.py
+++ object_detector_retinanet/keras_retinanet/utils/predict_iou.py
@@ -40,7 +40,8 @@ def predict(
     all_detections = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
     csv_data_lst = []
     csv_data_lst.append(['image_id', 'x1', 'y1', 'x2', 'y2', 'confidence', 'hard_score'])
-    result_dir = os.path.join(root_dir(), 'results')
+    # result_dir = os.path.join(root_dir(), 'results')
+    result_dir = os.path.join(generator.base_dir, "detection_results")
     create_folder(result_dir)
     timestamp = datetime.datetime.utcnow()
     res_file = result_dir + '/detections_output_iou_{}_{}.csv'.format(hard_score_rate, timestamp)
